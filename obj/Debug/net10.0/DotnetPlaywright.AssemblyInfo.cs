086554b56c463f2b6e13fa01996896f8ae5bfff4
Mani Movassagh <mani.mgh@gmail.com>
2025-09-15 21:07:14 +0200

    Clean up repository and add .gitignore

--- obj/Debug/net10.0/DotnetPlaywright.AssemblyInfo.cs
+++ obj/Debug/net10.0/DotnetPlaywright.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetPlaywright")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1461d3d9b40960e6a9b8b434b8007fc825143e30")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetPlaywright")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetPlaywright")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
